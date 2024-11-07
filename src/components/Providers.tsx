1a5aa12c70fe7820b755fae448c05f7595056b07
atneon27 <aryan.tiwari.at27@gmail.com>
2024-11-07 22:34:08 +0530

    Provider to keep track of state

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,14 +4,19 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider as NextThemeProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const Provider = ({children, ...props}: ThemeProviderProps) => {
-    return(
-        <NextThemeProvider attribute="class" defaultTheme="system" enableSystem>
-            <SessionProvider>
-                {children}
-            </SessionProvider>
-        </NextThemeProvider>
+const queryClient = new QueryClient();
+
+const Provider = ({ children, ...props }: ThemeProviderProps) => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <NextThemeProvider attribute="class" defaultTheme="system" enableSystem>
+                <SessionProvider>
+                    {children}
+                </SessionProvider>
+            </NextThemeProvider>
+        </QueryClientProvider>
     )
 }
 
