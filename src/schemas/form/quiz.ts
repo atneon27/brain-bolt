4ca7b6582857893c8acc990ed8b4fad82d250922
atneon27 <aryan.tiwari.at27@gmail.com>
2024-11-11 22:17:34 +0530

    add form schema

---
+++ src/schemas/form/quiz.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+export const QuizCreationSchema = z.object({
+    topic: z.string().min(4, {message: "Topic must be 4 characters long!"}).max(50, {message: "Topic exceeds permissable word limit!"}),
+    type: z.enum(['mcq', 'open_ended']),
+    amount: z.number().min(1).max(10),
+});
+
+
