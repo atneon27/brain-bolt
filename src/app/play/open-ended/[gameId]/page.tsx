9caec0e1ba3ada7b420f08abc3fd87c6b2bc2703
atneon27 <aryan.tiwari.at27@gmail.com>
2024-11-20 23:01:33 +0530

    added a page for open-ended questions

---
+++ src/app/play/open-ended/[gameId]/page.tsx
@@ -0,0 +1,24 @@
+import { getAuthSession } from '@/lib/nextauth'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+interface Props {
+    params: {
+        gameId: string
+    }
+}
+
+const OpenEndedPage = async ({params: {gameId}}: Props) => {
+    const session = await getAuthSession();
+    if(!session?.user) {
+        return redirect('/');
+    }
+
+    return (
+        <div>
+            {gameId}
+        </div>
+    )
+}
+
+export default OpenEndedPage;
