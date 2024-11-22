f5e197cb3da5f49af93323a1a2dced5576998f59
atneon27 <aryan.tiwari.at27@gmail.com>
2024-11-22 22:29:31 +0530

    Revert "updated migrations"
    This reverts commit 3b1bdccb05de4410cbf695e269600a1691bbce66.

--- prisma/migrations/20241106150351_init/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `options` column on the `Questions` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "Questions" DROP COLUMN "options",
-ADD COLUMN     "options" TEXT[];
